9a35a66be9ad5b00815c9017dbf3e1717e2ed669
verenichvlad <verenichvlad@gmail.com>
2015-10-26 21:42:12 +0100

    initial commit

---
+++ MVCTutorial2/DAL/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MVCTutorial1.Models;
+using  System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MVCTutorial1.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext") {}
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MVCTutorial2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCTutorial1.MvcApplication" Language="C#" %>
